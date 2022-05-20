722c06fabaca6f85c2675bdb9b1b6ab8aabed01d
dluongo9 <54182801+dluongo9@users.noreply.github.com>
2022-05-20 14:02:06 -0700

    committed

--- src/run_hw7.sh
+++ src/run_hw7.sh
@@ -2,4 +2,4 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate /dropbox/21-22/575k/env
 python run.py
-python run.py --num_epochs 50 --hidden_dim 50 --lr .04
+python run.py --num_epochs 50 --hidden_dim 50 --lr .004
